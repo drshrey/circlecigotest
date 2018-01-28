a7969d47b911630267dd9e4b99ae5c907fdfa269
Shreyas Jaganmohan <shreyas.jaganmohan@gmail.com>
2018-01-28 13:29:37 -0800

    Add subtract fn

--- main_test.go
+++ main_test.go
@@ -6,5 +6,9 @@ import (
 )
 
 func TestAdd(t *testing.T) {
-	assert.Equal(t, 4, add(2, 2), "2 +2 = 4")
+	assert.Equal(t, 4, add(2, 2), "2 + 2 = 4")
+}
+
+func TestSubtract(t *testing.T) {
+	assert.Equal(t, 0, subtract(2, 2), "2 - 2 = 0")
 }
