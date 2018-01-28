54ebaddb41f09b64dce651a8b692fd7800544b04
Shreyas Jaganmohan <shreyas.jaganmohan@gmail.com>
2018-01-28 14:48:16 -0800

    Add subtract fn (#1)

--- main.go
+++ main.go
@@ -8,6 +8,11 @@ func add(x int, y int) int {
 	return x + y
 }
 
+func subtract(x int, y int) int {
+	return x - y
+}
+
 func main() {
 	fmt.Printf("2 + 2 = %d\n", add(2, 2))
+	fmt.Printf("2 - 2 = %d\n", subtract(2, 2))
 }
