cc4a67041a9944d38320a81c7c255c559c28dec2
Shreyas Jaganmohan <shreyas.jaganmohan@gmail.com>
2018-01-28 14:49:44 -0800

    Add db (#2)

--- main.go
+++ main.go
@@ -2,10 +2,37 @@ package main
 
 import (
 	"fmt"
+	"github.com/drshrey/circlecigotest/models"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 func add(x int, y int) int {
 	return x + y
+}
+
+func migrate() error {
+	db, err := gorm.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=%s password=%s",
+		"localhost",
+		"5432",
+		"drshrey",
+		"testdb",
+		"disable",
+		"",
+	))
+	if err != nil {
+		return err
+	}
+
+	db.AutoMigrate(
+		&models.Entry{},
+	)
+	return nil
+}
+
+func makeOther(db *gorm.DB, entry *models.Entry) {
+	entry.Property = !entry.Property
+	db.Save(entry)
 }
 
 func subtract(x int, y int) int {
@@ -14,5 +41,42 @@ func subtract(x int, y int) int {
 
 func main() {
 	fmt.Printf("2 + 2 = %d\n", add(2, 2))
+	err := migrate()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	db, err := gorm.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=%s password=%s",
+		"localhost",
+		"5432",
+		"drshrey",
+		"testdb",
+		"disable",
+		"",
+	))
+	if err != nil {
+		fmt.Println("main: error could not connect to postgres")
+		return
+	}
+
+	// create entry
+	entry := models.Entry{
+		Property: false,
+	}
+
+	db.Create(&entry)
+
+	fmt.Println(entry.Property)
+	makeOther(db, &entry)
+	fmt.Println("changed", entry.Property)
+
+	// rows := []models.Entry{}
+	// if db.Find(&rows).RecordNotFound() {
+	// 	fmt.Println("no records found")
+	// } else {
+	// 	fmt.Println(rows)
+	// }
+
 	fmt.Printf("2 - 2 = %d\n", subtract(2, 2))
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"github.com/drshrey/circlecigotest/models"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -11,4 +15,38 @@ func TestAdd(t *testing.T) {
 
 func TestSubtract(t *testing.T) {
 	assert.Equal(t, 0, subtract(2, 2), "2 - 2 = 0")
+}
+
+func TestMakeOther(t *testing.T) {
+	err := migrate()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	db, err := gorm.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=%s password=%s",
+		"localhost",
+		"5432",
+		"drshrey",
+		"testdb",
+		"disable",
+		"",
+	))
+	if err != nil {
+		fmt.Println("main: error could not connect to postgres")
+		return
+	}
+
+	// create entry
+	entry := models.Entry{
+		Property: false,
+	}
+
+	db.Create(&entry)
+	assert.Equal(t, false, entry.Property)
+	makeOther(db, &entry)
+
+	modifiedEntry := models.Entry{}
+	db.Where("id = ?", entry.Model.ID.String()).Find(&modifiedEntry)
+	assert.Equal(t, true, modifiedEntry.Property)
 }
